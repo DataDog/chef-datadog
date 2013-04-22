b7026c05e7a92daa31dd1f8f23d1ef55a21ea183
Mike Fiedler <miketheman@gmail.com>
2013-04-22 17:40:43 -0400

    updated test suite for chefspec 1.0.0.rc1

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
 describe 'datadog::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'datadog::default' }
 
-  it 'should do nothing' do
+  context 'when converging this recipe' do
+
+    let(:chef_run) { ChefSpec::ChefRunner.new.converge 'datadog::default' }
+
+    it 'should do nothing' do
+    end
+
   end
 
 end
