8a5f314d5b42ab3cfaecb0b98de9e0c1e8491db9
Mike Fiedler <miketheman@gmail.com>
2013-05-01 13:12:39 -0400

    moves logging to debug, updates notification syntax

--- providers/ddmonitor.rb
+++ providers/ddmonitor.rb
@@ -5,25 +5,25 @@ def whyrun_supported?
 end
 
 action :add do
-  Chef::Log.info"Adding monitoring for #{new_resource.name}"
-  new_resource.updated_by_last_action(false)
+  Chef::Log.debug "Adding monitoring for #{new_resource.name}"
   template "/etc/dd-agent/conf.d/#{new_resource.name}.yaml" do
     owner "dd-agent"
     mode 00600
-    notifies :restart, resources(:service => "datadog-agent"), :delayed
     variables(
       :init_config => new_resource.init_config,
-      :instances => new_resource.instances
+      :instances   => new_resource.instances
     )
+    notifies :restart, "service[datadog-agent]", :delayed
   end
+  new_resource.updated_by_last_action(false)
 end
 
 action :remove do
   if ::File.exists?("/etc/dd-agent/conf.d/#{new_resource.name}.yaml")
-    Chef::Log.info "Removing #{new_resource.name} from /etc/dd-agent/conf.d/"
+    Chef::Log.debug "Removing #{new_resource.name} from /etc/dd-agent/conf.d/"
     file "/etc/dd-agent/conf.d/#{new_resource.name}.yaml" do
       action :delete
-      notifies :restart, resources(:service => "datadog-agent"), :delayed
+      notifies :restart, "service[datadog-agent]", :delayed
     end
     new_resource.updated_by_last_action(true)
   end
