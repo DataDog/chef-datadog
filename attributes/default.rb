3de04163493080413915a90b30739dfff9d30dce
Mike Fiedler <miketheman@gmail.com>
2013-05-02 13:33:47 -0400

    more inline cookbook documentation

--- attributes/default.rb
+++ attributes/default.rb
@@ -73,6 +73,6 @@ default['datadog']['syslog']['udp'] = false
 default['datadog']['syslog']['host'] = nil
 default['datadog']['syslog']['port'] = nil
 
-#
-# For service-specific configuration, see /etc/dd-agent/conf.d
-#
+# For service-specific configuration, use the integration recipes included
+# in this cookbook, and apply them to the appropirate node's run list.
+# Read more at http://docs.datadoghq.com/
