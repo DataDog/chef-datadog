cfe9ac902c90d3f17d040f6a30bd7c53d3485eeb
Mike Fiedler <miketheman@gmail.com>
2013-05-02 21:19:10 -0400

    updates datadog::nginx recipe

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -1,5 +1,22 @@
-include_recipe "datadog::dd-agent"
+include_recipe 'datadog::dd-agent'
 
-datadog_ddmonitor "nginx" do
-  instances node["datadog"]["nginx"]["instances"]
+# Monitor nginx
+#
+# Assuming you have 2 instances "prod" and "test", you will need to set
+# up the following attributes at some point in your Chef run, in either
+# a role or another cookbook.
+#
+# node['datadog']['nginx']['instances'] = [
+#   {
+#     'nginx_status_url' => "http://localhost:81/nginx_status/",
+#     'tags' => ["prod"]
+#   },
+#   {
+#     'nginx_status_url' => "http://localhost:82/nginx_status/",
+#     'name' => ["test"]
+#   }
+# ]
+
+datadog_monitor 'nginx' do
+  instances node['datadog']['nginx']['instances']
 end
