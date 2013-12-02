56a8bcd66904b9a43dbc928efed8e28d16e01189
David Radcliffe <radcliffe.david@gmail.com>
2013-12-02 08:58:35 -0500

    mongo recipe should install pymongo

--- recipes/mongo.rb
+++ recipes/mongo.rb
@@ -1,5 +1,18 @@
 include_recipe "datadog::dd-agent"
 
+# Monitor mongo
+#
+# node.set['datadog']['mongo']['instances'] = [
+#   {
+#     'host' => 'localhost',
+#     'port' => '27017'
+#   }
+# ]
+
+package "python-setuptools"
+
+easy_install_package "pymongo"
+
 datadog_monitor "mongo" do
   instances node["datadog"]["mongo"]["instances"]
 end
