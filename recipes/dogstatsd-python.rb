a7da7aebbbbbd9845521142c2e2b2e1b782f1a1b
Mike Fiedler <miketheman@gmail.com>
2014-03-21 13:49:34 -0400

    copyright update for 2014

--- recipes/dogstatsd-python.rb
+++ recipes/dogstatsd-python.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: datadog
 # Recipe:: dogstatsd-python
 #
-# Copyright 2013, Datadog
+# Copyright 2013-2014, Datadog
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
