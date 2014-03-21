ccdf6053ead4b3dcb9749975e8a471b1fa3a0eb7
Mike Fiedler <miketheman@gmail.com>
2014-03-21 12:40:54 -0400

    update network check for single instance
    As reported in #98, it is possible to abuse attributes in a manner that
    the netowkr check will contain multiple instances.
    This change will raise the problem to the user as a WARN, but still
    enable them to render an incorrect file, as we don't know which
    attribute invocation they would want.
    
    A simpler workaround is to use override_attributes where necessary, as
    the default_attributes may be getting appended to the exisiting default
    array, as definced in the attributes/network.rb file.
    
    Fixes #98

--- recipes/network.rb
+++ recipes/network.rb
@@ -1,13 +1,15 @@
 include_recipe "datadog::dd-agent"
 
 # Monitor network
-# 
+#
 # node.datadog.network.instances = [
 #   {
 #     :collect_connection_state => "false",
 #     :excluded_interfaces => ["lo","lo0"]
 #   },
 # ]
+
+Chef::Log.warn 'Datadog network check only supports a single `instance`, please check attribute assignments' if node['datadog']['network']['instances'].count > 1
 
 datadog_monitor "network" do
   instances node["datadog"]["network"]["instances"]
