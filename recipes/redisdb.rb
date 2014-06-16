bd753f61bd42093190286a450d3a8dad9ef31f8f
Bill Broach <while1eq1@gmail.com>
2014-06-16 17:30:58 -0400

    Added example usage like in the rest of recipes/*

--- recipes/redisdb.rb
+++ recipes/redisdb.rb
@@ -1,5 +1,20 @@
 include_recipe "datadog::dd-agent"
 
+# Monitor redis
+#
+# Assuming you have 1 instance "prod"  on a given server, you will need to set
+# up the following attributes at some point in your Chef run, in either
+# a role or another cookbook.
+#
+# node['datadog']['redisdb']['instances'] = [
+#   {
+#     'server' => "localhost",
+#     'port' => 6379,
+#     'password' => "my_password",
+#     'tags' => ["prod"],
+#   }
+# ]
+
 # same name across distributions
 case node["platform_family"]
 when "debian"
