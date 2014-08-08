9ef6a5b8d22dfd7b7ee55170ec914a9a2b39465a
Benjamin Abbott-Scott <babbottscott@zendesk.com>
2014-08-08 00:35:30 +0000

    v1.2.1
    stupid apt package resource.  stupid aptitude.

--- recipes/dd-agent.rb
+++ recipes/dd-agent.rb
@@ -41,11 +41,13 @@ end
 
 if node['datadog']['install_base']
   package "datadog-agent-base" do
+    options "--force-yes" if node['datadog']['agent_version']
     version node['datadog']['agent_version']
     action node['datadog']['agent_version'] ? :upgrade : :install
   end
 else
   package "datadog-agent" do
+    options "--force-yes" if node['datadog']['agent_version']
     version node['datadog']['agent_version']
     action node['datadog']['agent_version'] ? :upgrade : :install
   end
