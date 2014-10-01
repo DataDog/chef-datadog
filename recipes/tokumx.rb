cb83dbeefdff91ee077fec81042742065fbd30f7
Ankur Chauhan <ankurtrojan@gmail.com>
2014-10-01 13:34:43 -0700

    Update tokumx.rb

--- recipes/tokumx.rb
+++ recipes/tokumx.rb
@@ -10,10 +10,6 @@ include_recipe "datadog::dd-agent"
 #   }
 # ]
 
-package "python-setuptools"
-
-easy_install_package "pymongo"
-
 datadog_monitor "tokumx" do
   instances node["datadog"]["tokumx"]["instances"]
 end
