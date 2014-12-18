5c90d833258c52d308418769ecaf30611ba5a92f
Justin Schuhmann <justin.schuhmann@e-hps.com>
2014-12-18 17:32:52 -0500

    added windows agent installation support

---
+++ libraries/helper.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: datadog
+# Library:: helper
+#
+# Author:: Justin Schuhmann <jmschu02@gmail.com>
+#
+
+
+if RUBY_PLATFORM =~ /mswin|mingw32|windows/
+  require 'chef/win32/version'
+end
+
+module Datadog
+  module Helper
+    def self.older_than_windows2003r2?
+      if RUBY_PLATFORM =~ /mswin|mingw32|windows/
+        win_version = Chef::ReservedNames::Win32::Version.new
+          win_version.windows_server_2003_r2? ||
+          win_version.windows_home_server? ||
+          win_version.windows_server_2003? ||
+          win_version.windows_xp? ||
+          win_version.windows_2000?
+      end
+    end
+  end
+end
