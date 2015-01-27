bdc40a67bf84232b271d2a2206153c79712d5c63
Mike Fiedler <miketheman@gmail.com>
2015-01-27 10:19:38 -0500

    :sparkles: it's a new year! :sparkles:
    [ci skip]

--- recipes/dd-agent.rb
+++ recipes/dd-agent.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: datadog
 # Recipe:: dd-agent
 #
-# Copyright 2011-2014, Datadog
+# Copyright 2011-2015, Datadog
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- recipes/repository.rb
+++ recipes/repository.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: datadog
 # Recipe:: repository
 #
-# Copyright 2013-2014, Datadog
+# Copyright 2013-2015, Datadog
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
