68fcc49c3d5b9def819aaa6cc02bea80edb2b6bf
Mike Fiedler <miketheman@gmail.com>
2015-01-31 09:47:57 -0500

    forgot to actually commit the tests for 6f29a4cd8a6aced8b73d865476bc2fac23905b59

---
+++ test/integration/datadog_apache/serverspec/apache_spec.rb
@@ -0,0 +1,35 @@
+# Encoding: utf-8
+require 'json_spec'
+require 'serverspec'
+require 'yaml'
+
+set :backend, :exec
+set :path, '/sbin:/usr/local/sbin:$PATH'
+
+AGENT_CONFIG = '/etc/dd-agent/conf.d/apache.yaml'
+
+describe service('datadog-agent') do
+  it { should be_running }
+end
+
+describe file(AGENT_CONFIG) do
+  it { should be_a_file }
+
+  it 'is valid yaml matching input values' do
+    generated = YAML.load_file(AGENT_CONFIG)
+
+    expected = {
+      'instances' => [
+        {
+          'apache_status_url' => 'http://mysite.com/server-status?auto',
+          'apache_user' => 'someusername',
+          'apache_password' => 'sekret',
+          'tags' => ['kitchen', 'sink']
+        }
+      ],
+      'init_config' => nil
+    }
+
+    expect(generated.to_json).to be_json_eql expected.to_json
+  end
+end
