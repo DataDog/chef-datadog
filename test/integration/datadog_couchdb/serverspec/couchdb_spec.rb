9f86c1b7801397f13c968b8be38ec95a0d2c4c46
Mike Fiedler <miketheman@gmail.com>
2015-01-31 17:54:07 -0500

    update couchdb template, recipe, tests
    Refs #154

---
+++ test/integration/datadog_couchdb/serverspec/couchdb_spec.rb
@@ -0,0 +1,34 @@
+# Encoding: utf-8
+require 'json_spec'
+require 'serverspec'
+require 'yaml'
+
+set :backend, :exec
+set :path, '/sbin:/usr/local/sbin:$PATH'
+
+AGENT_CONFIG = '/etc/dd-agent/conf.d/couch.yaml'
+
+describe service('datadog-agent') do
+  it { should be_running }
+end
+
+describe file(AGENT_CONFIG) do
+  it { should be_a_file }
+
+  it 'is valid yaml matching input values' do
+    generated = YAML.load_file(AGENT_CONFIG)
+
+    expected = {
+      'instances' => [
+        {
+          password: 'somepass',
+          server: 'http://localhost:5984',
+          user: 'someuser'
+        }
+      ],
+      'init_config' => nil
+    }
+
+    expect(generated.to_json).to be_json_eql expected.to_json
+  end
+end
