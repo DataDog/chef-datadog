4bc1513c0b558e133e8245018ebeee9c8a48e175
Or Cohen <orc@fewbytes.com>
2015-02-17 13:30:10 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'package@datadoghq.com'
 license          'Apache 2.0'
 description      'Installs/Configures datadog components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.1.0'
 
 %w(
   amazon
