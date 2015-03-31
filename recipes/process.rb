f048bbb62b16f69cfa1fca06228b3a93fc7152f4
jpcallanta <john@cnevids.com>
2015-03-31 11:30:36 -0400

    Add process recipe and process template file for process monitoring
    Includes serverspec test and kitchen config
    
    Fixes #112
    Closes #124, #181, #190
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

---
+++ recipes/process.rb
@@ -0,0 +1,29 @@
+include_recipe 'datadog::dd-agent'
+
+# Monitor processes
+#
+#    name:          (mandatory) STRING Is the display name of the check
+#    search_string: (mandatory) LIST OF STRINGS The strings to search for in process names. If
+#                               one of these matches a process's name, that process will be
+#                               included in the stats.
+#    exact_match:   (optional)  True/False, defaults to True, if you want to look for a partial
+#                               match, use exact_match: "False", otherwise use the exact base
+#                               name of the process.
+#
+# Example:
+#
+# node.datadog.process.instances = [
+#   {
+#     :name => "ssh",
+#     :search_string => ["ssh","sshd"],
+#     :exact_match => "False",
+#   },
+#   {
+#     :name => "postgres",
+#     :search_string => ["postgres"],
+#   },
+# ]
+
+datadog_monitor 'process' do
+  instances node['datadog']['process']['instances']
+end
