132e295d6058984ab443bd92cb040ee62e4a74a3
Mike Fiedler <miketheman@gmail.com>
2015-04-07 23:48:32 +0300

    update postgres recipe example, add todos
    For next major revision, we should move all templates to use the new style of
    templating, to allow for open-ended configuration.
    
    Fixes #140 [ci skip]

--- recipes/postgres.rb
+++ recipes/postgres.rb
@@ -1,26 +1,26 @@
 include_recipe 'datadog::dd-agent'
 
-# Monitor postgres
-#
-# Simply declare the following attributes
-# One instance per server.
-#
-# node['datadog']['postgres']['instances'] = [
-#   {
-#     'server' => "localhost",
-#     'port' => "5432",
-#     'username' => "datadog",
-#     'tags' => ["test"]
-#   },
-#   {
-#     'server' => "remote",
-#     'port' => "5432",
-#     'username' => "datadog",
-#     'tags' => ["prod"],
-#     'dbname' => 'my_database',
-#     'relations' => ["apple_table", "orange_table"]
-#   }
-# ]
+# Build a data structure with configuration.
+# @see https://github.com/DataDog/dd-agent/blob/master/conf.d/postgres.yaml.example PostgreSQL Example
+# @example
+#   node.override['datadog']['postgres']['instances'] = [
+#     {
+#       'server' => "localhost",
+#       'port' => "5432",
+#       'username' => "datadog",
+#       'tags' => ["test"]
+#     },
+#     {
+#       'server' => "remote",
+#       'port' => "5432",
+#       'username' => "datadog",
+#       'tags' => ["prod"],
+#       'dbname' => 'my_database',
+#       'relations' => ["apple_table", "orange_table"]
+#     }
+#   ]
+# @todo Breaking, major version, convert `server` to `host` to match the check input.
+# @todo Breaking, major version, convert template to yaml-style
 
 datadog_monitor 'postgres' do
   instances node['datadog']['postgres']['instances']
