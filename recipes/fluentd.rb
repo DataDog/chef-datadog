d9114e0533a9d8347bc60b62d97a1cae1295c8f9
Yohei Kawahara <kawahara@cloudpack.jp>
2015-04-25 03:33:30 +0900

    change node to node.override and added comma

--- recipes/fluentd.rb
+++ recipes/fluentd.rb
@@ -2,10 +2,10 @@ include_recipe 'datadog::dd-agent'
 
 # Monitor fluentd
 #
-# node['datadog']['fluentd']['instances'] = [
+# node.override['datadog']['fluentd']['instances'] = [
 #   {
 #     'monitor_agent_url' => "http://example.com:24220/api/plugins.json",
-#     'plugin_ids' => ["plg1", "plg2"]
+#     'plugin_ids' => ["plg1", "plg2"],
 #     'tags' => ["tag1", "tag2"]
 #   }
 # ]
