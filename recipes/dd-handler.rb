ed3e8756e3a6abb9a1866ee680974485b88f785a
Jonathan Morley <morley.jonathan@gmail.com>
2015-09-22 14:02:37 -0400

    Rearrange order of chef_handler gem arguments

--- recipes/dd-handler.rb
+++ recipes/dd-handler.rb
@@ -34,8 +34,8 @@ chef_handler 'Chef::Handler::Datadog' do
   arguments [
     :api_key => node['datadog']['api_key'],
     :application_key => node['datadog']['application_key'],
-    :use_ec2_instance_id => node['datadog']['use_ec2_instance_id'],
-    :notify_on_failure => node['datadog']['handler']['notify_on_failure']
+    :notify_on_failure => node['datadog']['handler']['notify_on_failure'],
+    :use_ec2_instance_id => node['datadog']['use_ec2_instance_id']
   ]
   supports :report => true, :exception => true
   action :nothing
