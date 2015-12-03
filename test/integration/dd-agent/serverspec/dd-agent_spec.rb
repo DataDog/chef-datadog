3ee70037a652cbc817c2e9062362a19c512056ab
Montague, Brent <bmontague@cvent.com>
2015-12-03 09:41:50 -0500

    Remove test case for testing msi installed globally
    Believe this changes for each version so its not a maintainable check

--- test/integration/dd-agent/serverspec/dd-agent_spec.rb
+++ test/integration/dd-agent/serverspec/dd-agent_spec.rb
@@ -8,11 +8,6 @@ describe service(@agent_service_name) do
   it { should be_running }
 end
 
-# verifies that the agent is installed per-machine for windows
-describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products\776BC1A520EDC164EBF87A0875775D9C'), :if => os[:family] == 'windows' do
-  it { should exist }
-end
-
 describe command('/etc/init.d/datadog-agent info'), :if => os[:family] != 'windows' do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should contain 'OK' }
