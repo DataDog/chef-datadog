deb8ec93b25f31ec671d37ed35f2e13b157e990e
Cobus Bernard <cobusbernard@gmail.com>
2016-03-05 08:49:28 +0200

    Adding support for Marathon.

---
+++ recipes/marathon.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook Name:: datadog
+# Recipe:: marathon
+#
+# Copyright 2011-2015, Datadog
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+include_recipe 'datadog::dd-agent'
+
+# Build a data structure with configuration.
+# @example
+#   node.override['datadog']['marathon'] =
+#     instances: [{
+#       url: 'https://server:port',
+#       timeout: 8,
+#       tags: [
+#         'optional_tag1',
+#         'optional_tag2'
+#       ]
+#     }],
+#     init_config: {
+#       default_timeout: 5
+#     }
+
+datadog_monitor 'marathon' do
+  init_config node['datadog']['marathon']['init_config']
+  instances node['datadog']['marathon']['instances']
+end
---
+++ test/integration/datadog_marathon/serverspec/marathon_spec.rb
@@ -0,0 +1,31 @@
+# Encoding: utf-8
+require 'spec_helper'
+
+AGENT_CONFIG = File.join(@agent_config_dir, 'conf.d/marathon.yaml')
+
+describe service(@agent_service_name) do
+  it { should be_running }
+end
+
+describe file(AGENT_CONFIG) do
+  it { should be_a_file }
+
+  it 'is valid yaml matching input values' do
+    generated = YAML.load_file(AGENT_CONFIG)
+
+    expected = {
+      instances: [
+        {
+          url: 'localhost:8080',
+          timeout: 8,
+          tags: ['toto', 'tata']
+        }
+      ],
+      init_config: {
+        default_timeout: 10
+      }
+    }
+
+    expect(generated.to_json).to be_json_eql expected.to_json
+  end
+end
