4b16212bbca33edc4c39b4b98e02f7046cf00afd
Jean Rougé <jer329@cornell.edu>
2016-04-15 08:31:53 -0700

    Adding a datadog::ssh_check recipe to leverage SSH checks (#262)
    With the corresponding kitchen test.

---
+++ recipes/ssh_check.rb
@@ -0,0 +1,24 @@
+include_recipe 'datadog::dd-agent'
+
+# Monitor SSH connectivity
+#
+# Assuming you have SSH connections you want to monitor, you will need to set
+# up the following attributes at some point in your Chef run, in either a role
+# or another cookbook.
+#
+# node['datadog']['ssh_check']['instances'] = [
+#   {
+#     'host'              => '1.2.3.4',               # required for each instance
+#     'username'          => 'user',                  # required for each instance
+#     'password'          => 'password',              # optional
+#     'port'              => 22,                      # optional, defaults to 22
+#     'sftp_check'        => true,                    # optional, defaults to true
+#     'private_key_file'  => '/path/to/key',          # optional
+#     'add_missing_keys'  => false,                   # optional, defaults to false
+#     'tags'              => [node.chef_environment]  # optional
+#   }
+# ]
+
+datadog_monitor 'ssh_check' do
+  instances node['datadog']['ssh_check']['instances']
+end
---
+++ test/integration/datadog_ssh_check/serverspec/ssh_check_spec.rb
@@ -0,0 +1,44 @@
+# Encoding: utf-8
+require 'json_spec'
+require 'serverspec'
+require 'yaml'
+
+set :backend, :exec
+set :path, '/sbin:/usr/local/sbin:$PATH'
+
+AGENT_CONFIG = '/etc/dd-agent/conf.d/ssh_check.yaml'
+
+describe service('datadog-agent') do
+  it { should be_running }
+end
+
+describe file(AGENT_CONFIG) do
+  it { should be_a_file }
+
+  it 'is valid yaml matching input values' do
+    generated = YAML.load_file(AGENT_CONFIG)
+
+    expected = {
+      'instances' => [
+        {
+          'host' => 'localhost',
+          'username' => 'root',
+          'password' => 'password',
+          'add_missing_keys' => false,
+          'tags' => ['tag1', 'tag2']
+        },
+        {
+          'host' => 'sftp_server.example.com',
+          'username' => 'test',
+          'port' => 2323,
+          'sftp_check' => true,
+          'private_key_file' => '/path/to/key',
+          'tags' => ['tag1', 'tag3']
+        }
+      ],
+      'init_config' => nil
+    }
+
+    expect(generated.to_json).to be_json_eql expected.to_json
+  end
+end
