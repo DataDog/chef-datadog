92956da47306e51863e31922eb215a555f8c7e96
Mike Fiedler <miketheman@gmail.com>
2016-04-16 17:06:26 -0400

    Convert kitchen test to spec test for activemq
    Instead of using an expensive kitchen test to validate a template
    rendering, use ChefSpec instead.
    
    Update the template for the simpler, extensible hash syntax and fix
    inline docs.

---
+++ spec/integrations/activemq_spec.rb
@@ -0,0 +1,99 @@
+describe 'datadog::activemq' do
+  expected_yaml = <<-EOF
+    instances:
+    - host: localhost
+      port: 1099
+      user: someuser
+      password: somepass
+      name: activemq_instance
+    init_config:
+      conf:
+        - include:
+            Type: Queue
+            attribute:
+              AverageEnqueueTime:
+                alias: activemq.queue.avg_enqueue_time
+                metric_type: gauge
+              ConsumerCount:
+                alias: activemq.queue.consumer_count
+                metric_type: gauge
+              ProducerCount:
+                alias: activemq.queue.producer_count
+                metric_type: gauge
+              MaxEnqueueTime:
+                alias: activemq.queue.max_enqueue_time
+                metric_type: gauge
+              MinEnqueueTime:
+                alias: activemq.queue.min_enqueue_time
+                metric_type: gauge
+              MemoryPercentUsage:
+                alias: activemq.queue.memory_pct
+                metric_type: gauge
+              QueueSize:
+                alias: activemq.queue.size
+                metric_type: gauge
+              DequeueCount:
+                alias: activemq.queue.dequeue_count
+                metric_type: counter
+              DispatchCount:
+                alias: activemq.queue.dispatch_count
+                metric_type: counter
+              EnqueueCount:
+                alias: activemq.queue.enqueue_count
+                metric_type: counter
+              ExpiredCount:
+                alias: activemq.queue.expired_count
+                metric_type: counter
+              InFlightCount:
+                alias: activemq.queue.in_flight_count
+                metric_type: counter
+
+        - include:
+            Type: Broker
+            attribute:
+              StorePercentUsage:
+                alias: activemq.broker.store_pct
+                metric_type: gauge
+              TempPercentUsage:
+                alias: activemq.broker.temp_pct
+                metric_type: gauge
+              MemoryPercentUsage:
+                alias: activemq.broker.memory_pct
+                metric_type: gauge
+  EOF
+
+  cached(:chef_run) do
+    ChefSpec::SoloRunner.new(step_into: ['datadog_monitor']) do |node|
+      node.automatic['languages'] = { python: { version: '2.7.2' } }
+
+      node.set['datadog'] = {
+        api_key: 'someapikey',
+        activemq: {
+          instances: [
+            {
+              host: 'localhost',
+              name: 'activemq_instance',
+              password: 'somepass',
+              port: 1099,
+              user: 'someuser'
+            }
+          ]
+        }
+      }
+    end.converge(described_recipe)
+  end
+
+  subject { chef_run }
+
+  it_behaves_like 'datadog-agent'
+
+  it { is_expected.to include_recipe('datadog::dd-agent') }
+
+  it { is_expected.to add_datadog_monitor('activemq') }
+
+  it 'renders expected YAML config file' do
+    expect(chef_run).to render_file('/etc/dd-agent/conf.d/activemq.yaml').with_content { |content|
+      expect(YAML.load(content).to_json).to be_json_eql(YAML.load(expected_yaml).to_json)
+    }
+  end
+end
