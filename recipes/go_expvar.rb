f13871ccd61ca0862c1ed09c9df17607e4dfe0d4
Al Lefebvre <alain.lefebvre@lightspeedhq.com>
2016-04-21 12:39:37 -0400

    Updated go_expvar template & updated tests to ChefSpec

--- recipes/go_expvar.rb
+++ recipes/go_expvar.rb
@@ -1,12 +1,12 @@
 include_recipe 'datadog::dd-agent'
 
 # Monitor Go metrics exported via expvar
-#  node.datadog.go_expvar.instances = [
+#  node['datadog']['go_expvar']['instances'] = [
 #   {
 #     'expvar_url' => "http://localhost:8080/debug/vars",
 #     'tags' => [
 #        "application:my_go_app"
-#        ],
+#      ],
 #     'metrics' => [
 #       {
 #         'path' => 'test_metrics_name_1', 'alias' => 'go_expvar.test_metrics_name_1', 'type' => 'gauge'
@@ -19,5 +19,6 @@ include_recipe 'datadog::dd-agent'
 #  ]
 
 datadog_monitor 'go_expvar' do
+  init_config node['datadog']['go_expvar']['init_config']
   instances node['datadog']['go_expvar']['instances']
 end
