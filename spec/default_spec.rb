069f314ff8f9d58b5bb491a2fff6ae9f58de385b
Mike Fiedler <miketheman@gmail.com>
2016-04-24 12:06:00 +0300

    Add validation to default spec test
    This will actually validate that there are no resources run when doing
    nothing.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -5,6 +5,7 @@ describe 'datadog::default' do
     let(:chef_run) { ChefSpec::SoloRunner.converge described_recipe }
 
     it 'should do nothing' do
+      expect(chef_run.resource_collection.resources.count).to eq 0
     end
   end
 end
