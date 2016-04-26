933380af8081e56274cabdd603381455a3ccfb78
Seth Klein <seth@kindlyops.com>
2016-04-26 12:19:17 -0400

    Convert directory integration kitchen test to rspec

---
+++ spec/integrations/directory_spec.rb
@@ -0,0 +1,52 @@
+describe 'datadog::directory' do
+
+  expected_yaml = <<-EOF
+    init_config:
+    instances:
+      - directory: "/path/to/directory"
+        name: "tag_value"
+        dirtagname: "tag_dirname"
+        filetagname: "tag_filename"
+        filegauges: False
+        pattern: "*.log"
+        recursive: True
+EOF
+
+  cached(:chef_run) do
+    ChefSpec::SoloRunner.new(step_into: ['datadog_monitor']) do |node|
+      node.automatic['languages'] = { python: { version: '2.7.2' } }
+
+      node.set['datadog'] = {
+        api_key: 'someapikey',
+        directory: {
+          instances: [
+            {
+              directory: '/path/to/directory',
+              name: 'tag_value',
+              dirtagname: 'tag_dirname',
+              filetagname: 'tag_filename',
+              filegauges: false,
+              pattern: '*.log',
+              recursive: true,
+            }
+          ]
+        }
+      }
+    end.converge(described_recipe)
+  end
+
+  subject { chef_run }
+
+  it_behaves_like 'datadog-agent'
+
+  it { is_expected.to include_recipe('datadog::dd-agent') }
+
+  it { is_expected.to add_datadog_monitor('directory') }
+
+  it 'renders expected YAML config file' do
+    expect(chef_run).to render_file('/etc/dd-agent/conf.d/directory.yaml').with_content { |content|
+      expect(YAML.load(content).to_json).to be_json_eql(YAML.load(expected_yaml).to_json)
+    }
+  end
+
+end
