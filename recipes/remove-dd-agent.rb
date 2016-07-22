06d35e0dccf823f40fc8ef3c161b15f0c0354618
Ray Crawford <ray.crawford@gmail.com>
2016-07-22 00:17:57 -0400

    Enhance remove-dd-agent.rb
    Enhance to extend utility to include Windows.  Tested on Windows 2012 R2 and Windows 2008 R2 SP1.

--- recipes/remove-dd-agent.rb
+++ recipes/remove-dd-agent.rb
@@ -1,4 +1,11 @@
 # Run this recipe to completely remove the Datadog Agent
-package 'datadog-agent' do
-  action :purge
+case node['os']
+when 'linux'
+  package 'datadog-agent' do
+    action :purge
+  end
+when 'windows'
+  package 'Datadog Agent' do
+    action :remove
+  end
 end
