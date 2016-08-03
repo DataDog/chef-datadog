edd057f34a7099a291134da09c7f229ffb433f93
Bryan Stenson <bryan@revinate.com>
2016-08-03 10:54:38 -0700

    add support for tags in `etcd` check (#322)

---
+++ spec/integrations/etcd_spec.rb
@@ -0,0 +1,49 @@
+describe 'datadog::etcd' do
+  expected_yaml = <<-EOF
+    instances:
+    - url: localhost
+      ssl_keyfile: key_path
+      ssl_certfile: cert_path
+      ssl_cert_validation: true
+      ssl_ca_certs: ca_path
+      tags:
+      - mytag
+    init_config:
+  EOF
+
+  cached(:chef_run) do
+    ChefSpec::SoloRunner.new(step_into: ['datadog_monitor']) do |node|
+      node.automatic['languages'] = { python: { version: '2.7.2' } }
+
+      node.set['datadog'] = {
+        api_key: 'someapikey',
+        etcd: {
+          instances: [
+            {
+              url: 'localhost',
+              ssl_keyfile: 'key_path',
+              ssl_certfile: 'cert_path',
+              ssl_cert_validation: true,
+              ssl_ca_certs: 'ca_path',
+              tags: ['mytag']
+            }
+          ]
+        }
+      }
+    end.converge(described_recipe)
+  end
+
+  subject { chef_run }
+
+  it_behaves_like 'datadog-agent'
+
+  it { is_expected.to include_recipe('datadog::dd-agent') }
+
+  it { is_expected.to add_datadog_monitor('etcd') }
+
+  it 'renders expected YAML config file' do
+    expect(chef_run).to render_file('/etc/dd-agent/conf.d/etcd.yaml').with_content { |content|
+      expect(YAML.load(content).to_json).to be_json_eql(YAML.load(expected_yaml).to_json)
+    }
+  end
+end
