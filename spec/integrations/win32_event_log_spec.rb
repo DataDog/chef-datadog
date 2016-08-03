5d223af43fe36859c86dc5d3c7d553a6fdea05f3
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2016-08-03 19:52:49 +0200

    Add `win32_event_log` check support (#314)

---
+++ spec/integrations/win32_event_log_spec.rb
@@ -0,0 +1,56 @@
+describe 'datadog::win32_event_log' do
+  expected_yaml = <<-EOF
+  init_config:
+    tag_event_id: yes
+
+  instances:
+    - log_file:
+        - Application
+      source_name:
+        - MSSQLSERVER
+      type:
+        - Warning
+        - Error
+      message_filters:
+        - "%error%"
+      tags:
+        - sqlserver
+  EOF
+
+  cached(:chef_run) do
+    ChefSpec::SoloRunner.new(step_into: ['datadog_monitor']) do |node|
+      node.automatic['languages'] = { 'python' => { 'version' => '2.7.2' } }
+      node.set['datadog'] = {
+        api_key: 'someapikey',
+        win32_event_log: {
+          init_config: {
+            tag_event_id: true
+          },
+          instances: [
+            {
+              log_file: ['Application'],
+              source_name: ['MSSQLSERVER'],
+              type: ['Warning', 'Error'],
+              message_filters: ['%error%'],
+              tags: ['sqlserver']
+            }
+          ]
+        }
+      }
+    end.converge(described_recipe)
+  end
+
+  subject { chef_run }
+
+  it_behaves_like 'datadog-agent'
+
+  it { is_expected.to include_recipe('datadog::dd-agent') }
+
+  it { is_expected.to add_datadog_monitor('win32_event_log') }
+
+  it 'renders expected YAML config file' do
+    expect(chef_run).to render_file('/etc/dd-agent/conf.d/win32_event_log.yaml').with_content { |content|
+      expect(YAML.load(content).to_json).to be_json_eql(YAML.load(expected_yaml).to_json)
+    }
+  end
+end
