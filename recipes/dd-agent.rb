edaf370c93c5b629e8db672565779f7b1a3f27d5
Renato Covarrubias <renato.covarrubias@latam.com>
2016-09-13 11:58:32 -0300

    Apply rubocop

--- recipes/dd-agent.rb
+++ recipes/dd-agent.rb
@@ -42,7 +42,7 @@ directory node['datadog']['config_dir'] do
   else
     owner 'dd-agent'
     group 'root'
-    mode 0755
+    mode 0o755
   end
 end
 
@@ -61,7 +61,7 @@ template agent_config_file do
   else
     owner 'dd-agent'
     group 'root'
-    mode 0640
+    mode 0o640
   end
   variables(
     :api_key => node['datadog']['api_key'],
