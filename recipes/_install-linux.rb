247feacf60ae6a578331cd9cbdf4c1bbe99d03ba
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2016-09-14 17:51:30 +0200

    [dd-agent] Fix version test when version contains an epoch (#357)
    Previously the test wouldn't give the expected result for versions like
    `1:5.8.0:1` for instance (which is standard for our debian pkg).

--- recipes/_install-linux.rb
+++ recipes/_install-linux.rb
@@ -23,7 +23,7 @@ include_recipe 'datadog::repository' if node['datadog']['installrepo']
 dd_agent_version = node['datadog']['agent_version']
 
 # If version specified and lower than 5.x
-if !dd_agent_version.nil? && dd_agent_version.split('.')[0].to_i < 5
+if !dd_agent_version.nil? && dd_agent_version.split('.')[0].split(':').last.to_i < 5
   Chef::Log.warn 'Support for Agent pre 5.x will be removed in datadog cookbook version 3.0'
   # Select correct package name based on attribute
   dd_pkg_name = node['datadog']['install_base'] ? 'datadog-agent-base' : 'datadog-agent'
