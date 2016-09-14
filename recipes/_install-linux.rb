d504ce325f8af5214de3043990a3342b13646885
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2016-09-14 17:21:16 +0200

    [dd-agent][yum] Fix `datadog-agent-base` guard logic
    We want to remove the `datadog-agent-base` pkg only if it's installed,
    which is what the `rpm -q datadog-agent-base` cmd checks (it returns
    `0` when the pkg is installed, `1` otherwise).

--- recipes/_install-linux.rb
+++ recipes/_install-linux.rb
@@ -35,7 +35,7 @@ else
   # default behavior, remove the `base` package as it is no longer needed
   package 'datadog-agent-base' do
     action :remove
-    not_if 'rpm -q datadog-agent-base' if %w(rhel fedora).include?(node['platform_family'])
+    only_if 'rpm -q datadog-agent-base' if %w(rhel fedora).include?(node['platform_family'])
     not_if 'apt-cache policy datadog-agent-base | grep "Installed: (none)"' if node['platform_family'] == 'debian'
   end
   # Install the regular package
