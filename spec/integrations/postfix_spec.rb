6202b0d51577a87d1745d4fa41671d0be9225af3
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2016-11-14 20:05:16 -0500

    [postfix] Add spec test

---
+++ spec/integrations/postfix_spec.rb
@@ -0,0 +1,60 @@
+describe 'datadog::postfix' do
+  expected_yaml = <<-EOF
+    instances:
+    - directory: /var/spool/postfix
+      queues:
+      - incoming
+      - active
+      - deferred
+      tags:
+      - prod
+      - postfix_core
+    - directory: /var/spool/postfix
+      queues:
+      - bounce
+    init_config:
+  EOF
+
+  before do
+    allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).and_call_original
+    allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('sudo')
+    allow_any_instance_of(Chef::Recipe).to receive(:sudo)
+  end
+
+  cached(:chef_run) do
+    ChefSpec::SoloRunner.new(step_into: ['datadog_monitor']) do |node|
+      node.automatic['languages'] = { python: { version: '2.7.2' } }
+
+      node.set['datadog'] = {
+        api_key: 'someapikey',
+        postfix: {
+          instances: [
+            {
+              directory: '/var/spool/postfix',
+              queues: ['incoming', 'active', 'deferred'],
+              tags: ['prod', 'postfix_core']
+            },
+            {
+              directory: '/var/spool/postfix',
+              queues: ['bounce']
+            }
+          ]
+        }
+      }
+    end.converge(described_recipe)
+  end
+
+  subject { chef_run }
+
+  it_behaves_like 'datadog-agent'
+
+  it { is_expected.to include_recipe('datadog::dd-agent') }
+
+  it { is_expected.to add_datadog_monitor('postfix') }
+
+  it 'renders expected YAML config file' do
+    expect(chef_run).to render_file('/etc/dd-agent/conf.d/postfix.yaml').with_content { |content|
+      expect(YAML.load(content).to_json).to be_json_eql(YAML.load(expected_yaml).to_json)
+    }
+  end
+end
