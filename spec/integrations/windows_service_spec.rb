f6f923f895e9f8d30c2ab296f60eac49d7614699
mlcooper <mitchell.l.cooper@disney.com>
2017-01-04 10:17:14 -0500

    Add recipe for windows_service integration (#387)
    Fixes #386

---
+++ spec/integrations/windows_service_spec.rb
@@ -0,0 +1,93 @@
+describe 'datadog::windows_service' do
+  expected_yaml = <<-EOF
+    instances:
+    - host: REMOTEHOSTNAME
+      username: REMOTEHOSTNAME\\thomas
+      password: secretpw
+      services:
+        - RemoteService1
+        - RemoteService2
+
+    init_config:
+
+  EOF
+
+  cached(:chef_run) do
+    ChefSpec::SoloRunner.new(step_into: ['datadog_monitor']) do |node|
+      node.automatic['languages'] = { python: { version: '2.7.2' } }
+
+      node.set['datadog'] = {
+        api_key: 'someapikey',
+        windows_service: {
+          instances: [
+            {
+              username: 'REMOTEHOSTNAME\\thomas',
+              services: ['RemoteService1', 'RemoteService2'],
+              host: 'REMOTEHOSTNAME',
+              password: 'secretpw'
+            }
+          ]
+        }
+      }
+    end.converge(described_recipe)
+  end
+
+  subject { chef_run }
+
+  it_behaves_like 'datadog-agent'
+
+  it { is_expected.to include_recipe('datadog::dd-agent') }
+
+  it { is_expected.to add_datadog_monitor('windows_service') }
+
+  it 'renders expected YAML config file for remote host service monitoring' do
+    expect(chef_run).to render_file('/etc/dd-agent/conf.d/windows_service.yaml').with_content { |content|
+      expect(YAML.load(content).to_json).to be_json_eql(YAML.load(expected_yaml).to_json)
+    }
+  end
+end
+
+describe 'datadog::windows_service' do
+  expected_yaml = <<-EOF
+    instances:
+    - host: .
+      services:
+        - LocalService1
+        - LocalService2
+
+    init_config:
+
+  EOF
+
+  cached(:chef_run) do
+    ChefSpec::SoloRunner.new(step_into: ['datadog_monitor']) do |node|
+      node.automatic['languages'] = { python: { version: '2.7.2' } }
+
+      node.set['datadog'] = {
+        api_key: 'someapikey',
+        windows_service: {
+          instances: [
+            {
+              services: ['LocalService1', 'LocalService2'],
+              host: '.'
+            }
+          ]
+        }
+      }
+    end.converge(described_recipe)
+  end
+
+  subject { chef_run }
+
+  it_behaves_like 'datadog-agent'
+
+  it { is_expected.to include_recipe('datadog::dd-agent') }
+
+  it { is_expected.to add_datadog_monitor('windows_service') }
+
+  it 'renders expected YAML config file for local host service monitoring' do
+    expect(chef_run).to render_file('/etc/dd-agent/conf.d/windows_service.yaml').with_content { |content|
+      expect(YAML.load(content).to_json).to be_json_eql(YAML.load(expected_yaml).to_json)
+    }
+  end
+end
