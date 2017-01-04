a5108c0063a0d31223b360e5c0211c43150e29ad
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2017-01-04 18:16:06 +0100

    [monitor] Add some inline docs on monitor resource

--- resources/monitor.rb
+++ resources/monitor.rb
@@ -6,10 +6,13 @@ default_action :add if defined?(default_action)
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :cookbook, :kind_of => String, :default => 'datadog'
 
+# Attributes passed as variables to the yaml template
 # checks have 2 sections: init_config and instances
 # we mimic these here, no validation is performed until the template
 # is evaluated.
 attribute :init_config, :kind_of => Hash, :required => false, :default => {}
 attribute :instances, :kind_of => Array, :required => false, :default => []
+# Some yaml templates use the `version` to determine which default configuration to use
 attribute :version, :kind_of => Integer, :required => false, :default => nil
+
 attribute :use_integration_template, :kind_of => [TrueClass, FalseClass], :required => false, :default => false
