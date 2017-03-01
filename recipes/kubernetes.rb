39650ef464c63be3098a1048ed3bb150a1905197
Alexey Slobodiskiy <oslobod@softserveinc.com>
2017-03-01 19:51:05 +0200

    added kubernetes recipe

---
+++ recipes/kubernetes.rb
@@ -0,0 +1,30 @@
+include_recipe 'datadog::dd-agent'
+
+# Monitor kubernetes
+#
+# Assuming you have a kubernetes instance on a given server, you will need to set
+# up the following attributes at some point in your Chef run, in either
+# a role or another cookbook.
+#
+# node['datadog']['kubernetes']['instances'] = [
+#   {
+#     'host'           => 'localhost',
+#     'port'           => 4194,
+#     'method'         => 'http',
+#     'kubelet_port'   => 10255,
+#     'namespace'      => 'default'
+#     'collect_events' => false,
+#     'use_histogram'  => false,
+#     'enabled_rates'  => [
+#       'cpu.*',
+#       'network.*'
+#     ],
+#     'enabled_gauges' => [
+#       'filesystem.*'
+#     ]
+#   }
+# ]
+
+datadog_monitor 'kubernetes' do
+  instances node['datadog']['kubernetes']['instances']
+end
