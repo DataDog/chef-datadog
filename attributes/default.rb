3f6435b600e0556d6265f89b387cb0f367a1558a
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2017-03-23 15:23:52 -0400

    [windows] Add option to support EXE Agent installer (#410)
    * The EXE installer handles per-user to per-machine upgrades
    gracefully, including edges cases that the present cookbook can't
    reasonably handle on its own.
      For this reason, we add the option to use the exe installer when needed
    (i.e. for upgrades from Agent <= 5.10.1 to Agent >= 5.12.0).
    * The URL to the latest installer changes in 5.12.0 from `ddagent-cli`
    to `ddagent-cli-latest` so that unattended upgrades don't make chef
    runs fail.
      Reflect this here. The new URLs work only since 5.12.0, so this change
    should only be released once the 5.12.0 installers (msi and exe) are
    generally available at these URLs.
    * Made some changes on the package removal to handle `windows` >= 1.39
    more gracefully. Updated the spec tests assuming they run on Chef >= 12.6.
      This should be removed (and the whole recipe should be cleaned up)
    when we stop supporting Chef < 12.6 on Windows.
    * Added related documentation.

--- attributes/default.rb
+++ attributes/default.rb
@@ -111,6 +111,13 @@ default['datadog']['yumrepo_gpgkey_new'] = "#{yum_protocol}://yum.datadoghq.com/
 # Expected checksum to validate correct agent installer is downloaded (Windows only)
 default['datadog']['windows_agent_checksum'] = nil
 
+# Set to `true` to use the EXE installer on Windows, recommended to gracefully handle upgrades from per-user
+# to per-machine installs on most environments. We recommend setting this option to `true` for Agent upgrades from
+# versions <= 5.10.1 to versions >= 5.12.0.
+# The EXE installer exists since Agent release 5.12.0
+# If you're already using version >= 5.12.0 of the Agent, leave this to false.
+default['datadog']['windows_agent_use_exe'] = false
+
 # Values that differ on Windows
 # The location of the config folder (containing conf.d)
 # The name of the dd agent service
--- recipes/_install-windows.rb
+++ recipes/_install-windows.rb
@@ -24,29 +24,66 @@ dd_agent_version =
     node['datadog']['agent_version']
   end
 
-# If no version is specified, select the latest package
-dd_agent_msi = dd_agent_version ? "ddagent-cli-#{dd_agent_version}.msi" : 'ddagent-cli.msi'
-temp_file = ::File.join(Chef::Config[:file_cache_path], 'ddagent-cli.msi')
+# If no version is specified, select the latest package.
+# The latest package basename is `ddagent-cli-latest` since Agent version 5.12.0
+dd_agent_installer_basename = dd_agent_version ? "ddagent-cli-#{dd_agent_version}" : 'ddagent-cli-latest'
+temp_file_basename = ::File.join(Chef::Config[:file_cache_path], 'ddagent-cli')
+
+if node['datadog']['windows_agent_use_exe']
+  dd_agent_installer = "#{dd_agent_installer_basename}.exe"
+  temp_file = "#{temp_file_basename}.exe"
+  installer_type = :custom
+  install_options = '/q'
+else
+  dd_agent_installer = "#{dd_agent_installer_basename}.msi"
+  temp_file = "#{temp_file_basename}.msi"
+  installer_type = :msi
+  # Agent >= 5.12.0 installs per-machine by default, but specifying ALLUSERS=1 shouldn't affect the install
+  install_options = '/norestart ALLUSERS=1'
+end
+
+# Use a separate resource without a `source` so that it looks in the Registry to find the MSI to use
+# for the package removal
+# On Chef >= 12.6, we have to use the `package` resource instead of `windows_package` to allow not specifying a source
+# (this is related to the way the windows cookbook changes the provider of `windows_package` depending on the
+# version of Chef, and to improvements on the `package` resource made in Chef 12.6)
+# FIXME: when we remove Chef < 12.6 support on Windows, which should allow using core resources only
+use_windows_package_resource = Gem::Version.new(Chef::VERSION) < Gem::Version.new('12.6.0')
+if use_windows_package_resource
+  windows_package 'Datadog Agent removal' do
+    package_name 'Datadog Agent'
+    action :nothing
+  end
+else
+  package 'Datadog Agent removal' do
+    package_name 'Datadog Agent'
+    action :nothing
+  end
+end
 
 package_retries = node['datadog']['agent_package_retries']
 package_retry_delay = node['datadog']['agent_package_retry_delay']
 
 # Download the installer to a temp location
 remote_file temp_file do
-  source node['datadog']['windows_agent_url'] + dd_agent_msi
+  source node['datadog']['windows_agent_url'] + dd_agent_installer
   checksum node['datadog']['windows_agent_checksum'] if node['datadog']['windows_agent_checksum']
   retries package_retries unless package_retries.nil?
   retry_delay package_retry_delay unless package_retry_delay.nil?
   # As of v1.37, the windows cookbook doesn't upgrade the package if a newer version is downloaded
   # As a workaround uninstall the package first if a new MSI is downloaded
-  notifies :remove, 'windows_package[Datadog Agent]', :immediately
+  if use_windows_package_resource
+    notifies :remove, 'windows_package[Datadog Agent removal]', :immediately
+  else
+    notifies :remove, 'package[Datadog Agent removal]', :immediately
+  end
 end
 
 # Install the package
 windows_package 'Datadog Agent' do # ~FC009
   source temp_file
-  installer_type :msi
-  options '/norestart ALLUSERS=1'
+  installer_type installer_type
+  options install_options
   action :install
   success_codes [0, 3010]
 end
