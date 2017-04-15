1f7665c34239520ae3950f3dec0ebaca3fcef1f8
Mitchell Cooper <mitchell.l.cooper@disney.com>
2017-04-15 13:44:07 -0400

    Add sqlserver recipe

---
+++ recipes/sqlserver.rb
@@ -0,0 +1,66 @@
+include_recipe 'datadog::dd-agent'
+
+# see example configuration file here:
+# https://github.com/DataDog/integrations-core/blob/master/sqlserver/conf.yaml.example
+
+# Example 1 - Optionally, custom Metrics have been configured under init_config
+# node['datadog']['sqlserver'] =
+# {
+#   "instances": [
+#     {
+#       "host": "fakehostname,1433",
+#       "connector": "odbc",
+#       "driver": "SQL Server",
+#       "username": "fake_user",
+#       "password": "bogus_pw",
+#       "command_timeout": 30,
+#       "database": "fake_db_name",
+#       "tags": [
+#         "test_tag_name"
+#       ]
+#     }
+#   ],
+#   "init_config": {
+#     "custom_metrics": [
+#       {
+#         "name": "sqlserver.clr.execution",
+#         "counter_name": "CLR execution"
+#       },
+#       {
+#         "name": "sqlserver.exec.in_progress",
+#         "counter_name": "OLEDB calls",
+#         "instance_name": "Cumulative execution time (ms) per second"
+#       },
+#       {
+#         "name": "sqlserver.db.commit_table_entries",
+#         "counter_name": "Log Flushes/sec",
+#         "instance_name": "ALL",
+#         "tag_by": "db"
+#       }
+#     ]
+#   }
+# }
+
+# Example 2 - Optionally, custom Metrics have not been configured
+# node['datadog']['sqlserver'] =
+# {
+#   "instances": [
+#     {
+#       "host": "fakehostname,1433",
+#       "connector": "odbc",
+#       "driver": "SQL Server",
+#       "username": "fake_user",
+#       "password": "bogus_pw",
+#       "command_timeout": 30,
+#       "database": "fake_db_name",
+#       "tags": [
+#         "test_tag_name"
+#       ]
+#     }
+#   ]
+# }
+
+datadog_monitor 'sqlserver' do
+  init_config node['datadog']['sqlserver']['init_config']
+  instances node['datadog']['sqlserver']['instances']
+end
