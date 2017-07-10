7516a3254a66d33cad7bb78002731f92ac2f5811
Aaron Kalin <akalin@martinisoftware.com>
2017-07-10 11:14:07 -0500

    Cleanup metadata
    chef_version is now required on the supermarket and the source_url and
    issues_url are supported in Chef 12
    
    suggests is also never actually used by anything so I've removed it
    here

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache-2.0'
 description      'Installs/Configures datadog components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.12.0'
-chef_version     '>= 10.14' if respond_to? :chef_version
+chef_version     '>= 12.7' if respond_to? :chef_version
 source_url       'https://github.com/DataDog/chef-datadog' if respond_to? :source_url
 issues_url       'https://github.com/DataDog/chef-datadog/issues' if respond_to? :issues_url
 
@@ -26,8 +26,6 @@ depends          'apt' # We recommend '>= 2.1.0'. See CHANGELOG.md for details
 depends          'chef_handler', '>= 1.1' # We recommend '~> 1.3' with Chef < 12. See CHANGELOG.md for details
 depends          'windows' # We recommend '< 1.39.0' if running Chef >= 12.6. See README.md for details
 depends          'yum', '>= 3.0' # Use '~> 3.0' with Chef < 12
-
-suggests         'sudo' # ~FC052
 
 recipe 'datadog::default', 'Default'
 recipe 'datadog::dd-agent', 'Installs the Datadog Agent'
