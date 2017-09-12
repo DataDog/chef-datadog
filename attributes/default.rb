30fefca6b9a1aa180b64d58c2c4df141a7bb36c9
Conor Branagan <conor.branagan@gmail.com>
2017-09-11 21:33:33 -0400

    Add configuration for the process-agent
    Adds the flag for toggling the process-agent plus some additional
    optional configuration around the process-agent behavior.

--- attributes/default.rb
+++ attributes/default.rb
@@ -289,6 +289,38 @@ default['datadog']['ddtrace_python_version'] = nil
 # ddtrace ruby gem version
 default['datadog']['ddtrace_gem_version'] = nil
 
+# Live processes functionality settings
+# Set `enable_process_agent` to:
+# * `true` to explicitly enable the process agent
+# * `false` to explicitly disable it
+# Leave it to `nil` to let the agent's default behavior decide whether to run the process-agent
+default['datadog']['enable_process_agent'] = nil
+
+# Comma-separated list of regex patterns matching process commands to blacklist.
+# Example: 'my-secret-app,dbpass'
+default['datadog']['process_agent']['blacklist'] = nil
+
+# Comma-separated list of regex patterns of containers to include or skip.
+# Each pattern should be in the form of "field:pattern" where 'field' is either
+# 'image' or 'name'.
+# Example: 'image:redis,image:nginx'
+default['datadog']['process_agent']['container_blacklist'] = nil
+# Whitelist is applied after the blacklist.
+default['datadog']['process_agent']['container_whitelist'] = nil
+
+# Full path to store process-agent logs to override the default.
+default['datadog']['process_agent']['log_file'] = nil
+
+# If running in full process collection mode ('enable_process_agent' is true)
+# overrides the collection intervals for the full and real-time checks in seconds.
+default['datadog']['process_agent']['process_interval'] = nil
+default['datadog']['process_agent']['rtprocess_interval'] = nil
+
+# If only collecting containers ('enable_process_agent' is false but docker is available)
+# overrides the collection intervals for the full and real-time check.
+default['datadog']['process_agent']['container_interval'] = nil
+default['datadog']['process_agent']['rtcontainer_interval'] = nil
+
 # For custom gem servers on restricted networks
 # This attribute only works on Chef >= 12.3
 # Change false to the URL of your custom gem server
