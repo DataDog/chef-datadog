938fb7ede5fcf0ce9092e8136ad8e890266a2258
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2017-09-28 15:26:37 -0400

    Support of agent6 beta for debianoids (#472)
    Flipping the attribute 'datadog''agent6' to true installs the agent6
    instead of the agent5. Switching it back to false should reverse
    the operation as long as the agent 5 version is pinned.

--- providers/monitor.rb
+++ providers/monitor.rb
@@ -9,7 +9,8 @@ end
 
 action :add do
   Chef::Log.debug "Adding monitoring for #{new_resource.name}"
-  template ::File.join(node['datadog']['config_dir'], 'conf.d', "#{new_resource.name}.yaml") do
+  config_dir = node['datadog']['agent6'] ? node['datadog']['agent6_config_dir'] : node['datadog']['config_dir']
+  template ::File.join(config_dir, 'conf.d', "#{new_resource.name}.yaml") do
     if node['platform_family'] == 'windows'
       owner 'Administrators'
       rights :full_control, 'Administrators'
@@ -41,7 +42,8 @@ action :add do
 end
 
 action :remove do
-  confd_dir = ::File.join(node['datadog']['config_dir'], 'conf.d')
+  config_dir = node['datadog']['agent6'] ? node['datadog']['agent6_config_dir'] : node['datadog']['config_dir']
+  confd_dir = ::File.join(config_dir, 'conf.d')
   Chef::Log.debug "Removing #{new_resource.name} from #{confd_dir}"
   file ::File.join(confd_dir, "#{new_resource.name}.yaml") do
     action :delete
