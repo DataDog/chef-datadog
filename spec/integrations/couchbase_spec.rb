c1be2132fc89b8193c6178d744d1afd6ffa04d84
Greg Swallow <gswallow@gmail.com>
2017-10-17 11:10:01 -0400

    Add couchbase recipe.

---
+++ spec/integrations/couchbase_spec.rb
@@ -0,0 +1,42 @@
+describe 'datadog::couchbase' do
+  expected_yaml = <<-EOF
+    init_config:
+
+    instances:
+    - server: http://localhost:8091
+      user: Administrator
+      password: default
+  EOF
+
+  cached(:chef_run) do
+    ChefSpec::SoloRunner.new(step_into: ['datadog_monitor']) do |node|
+      node.automatic['languages'] = { 'python' => { 'version' => '2.7.2' } }
+      node.set['datadog'] = {
+        api_key: 'someapikey',
+        couchbase: {
+          instances: [
+            {
+              server: 'http://localhost:8091'
+              username: 'Administrator',
+              password: 'default'
+            }
+          ]
+        }
+      }
+    end.converge(described_recipe)
+  end
+
+  subject { chef_run }
+
+  it_behaves_like 'datadog-agent'
+
+  it { is_expected.to include_recipe('datadog::dd-agent') }
+
+  it { is_expected.to add_datadog_monitor('couchbase') }
+
+  it 'renders expected YAML config file' do
+    expect(chef_run).to(render_file('/etc/dd-agent/conf.d/couchbase.yaml').with_content { |content|
+      expect(YAML.safe_load(content).to_json).to be_json_eql(YAML.safe_load(expected_yaml).to_json)
+    })
+  end
+end
