ebbe85b7174a77ca5e84530ad83880e5f8776749
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2017-11-30 15:43:21 +0100

    [CI] Fix circleci (#491)
    * Use 32-char-long api key
    
    Otherwise the agent refuses to start now, and makes the chef client run
    fail
    
    * Remove API key error message from test

--- test/integration/dd-agent/serverspec/dd-agent_spec.rb
+++ test/integration/dd-agent/serverspec/dd-agent_spec.rb
@@ -8,7 +8,7 @@ describe service(@agent_service_name) do
   it { should be_running }
 end
 
-describe command('/etc/init.d/datadog-agent info'), :if => os[:family] != 'windows' do
+describe command('/etc/init.d/datadog-agent info | grep -v "API Key is invalid"'), :if => os[:family] != 'windows' do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should contain 'OK' }
   its(:stdout) { should_not contain 'ERROR' }
