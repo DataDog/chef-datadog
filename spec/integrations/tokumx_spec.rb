87f57799cb45d040cfd1ad324c361f3a7e3a9828
Greg Swallow <gswallow@gmail.com>
2017-11-30 05:07:27 -0600

    Add a tokumx recipe, which about three people will use. (#486)

---
+++ spec/integrations/tokumx_spec.rb
@@ -0,0 +1,43 @@
+describe 'datadog::tokumx' do
+  expected_yaml = <<-EOF
+    init_config:
+
+    instances:
+    - server: mongodb://localhost:27017
+      tags:
+      - 'env:test'
+
+  EOF
+
+  cached(:chef_run) do
+    ChefSpec::SoloRunner.new(step_into: ['datadog_monitor']) do |node|
+      node.automatic['languages'] = { 'python' => { 'version' => '2.7.2' } }
+      node.set['datadog'] = {
+        api_key: 'someapikey',
+        tokumx: {
+          instances: [
+            {
+              host: 'localhost',
+              port: '27017',
+              tags: ['env:test']
+            }
+          ]
+        }
+      }
+    end.converge(described_recipe)
+  end
+
+  subject { chef_run }
+
+  it_behaves_like 'datadog-agent'
+
+  it { is_expected.to include_recipe('datadog::dd-agent') }
+
+  it { is_expected.to add_datadog_monitor('tokumx') }
+
+  it 'renders expected YAML config file' do
+    expect(chef_run).to(render_file('/etc/dd-agent/conf.d/tokumx.yaml').with_content { |content|
+      expect(YAML.safe_load(content).to_json).to be_json_eql(YAML.safe_load(expected_yaml).to_json)
+    })
+  end
+end
