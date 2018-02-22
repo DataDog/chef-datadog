27d91a7768184f1d70ca3d9b34cd66fed3e77472
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2018-02-22 21:04:13 +0100

    [cassandra] Add updated metrics for Cassandra 2 and 3.x
    When the version attribute is set to 3. We don't update the metrics
    when the version attribute is set to 2 to avoid changing the metrics
    collected on existing Agent installs.

--- spec/integrations/cassandra_spec.rb
+++ spec/integrations/cassandra_spec.rb
@@ -362,4 +362,478 @@ describe 'datadog::cassandra' do
       })
     end
   end
+
+  context 'version 3' do
+    expected_yaml = <<-EOF
+    logs: ~
+    instances:
+      - host: localhost
+        port: 7199
+        cassandra_aliasing: true
+        user: someuser
+        password: somepass
+        process_name_regex: .*cassandra.*
+    init_config:
+      conf:
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: ClientRequest
+            name:
+              - Latency
+            attribute:
+              - 75thPercentile
+              - 95thPercentile
+              - OneMinuteRate
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: DroppedMessage
+            name:
+              - Dropped
+            attribute:
+              - OneMinuteRate
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: ThreadPools
+            scope:
+              - MutationStage
+              - CounterMutationStage
+              - ReadStage
+              - ViewMutationStage
+            name:
+              - PendingTasks
+              - CurrentlyBlockedTasks
+            path:
+              - request
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: ThreadPools
+            scope:
+              - MemtableFlushWriter
+              - HintsDispatcher
+              - MemtablePostFlush
+              - MigrationStage
+              - MiscStage
+              - SecondaryIndexManagement
+            name:
+              - PendingTasks
+              - CurrentlyBlockedTasks
+            path:
+              - internal
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: Storage
+            name:
+              - Load
+              - Exceptions
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: Table
+            bean_regex:
+              - .*keyspace=.*
+            name:
+              - ReadLatency
+              - WriteLatency
+            attribute:
+              - 75thPercentile
+              - 95thPercentile
+              - 99thPercentile
+              - OneMinuteRate
+          exclude:
+            keyspace:
+              - system
+              - system_auth
+              - system_distributed
+              - system_schema
+              - system_traces
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: Table
+            bean_regex:
+              - .*keyspace=.*
+            name:
+              - RangeLatency
+              - CasPrepareLatency
+              - CasProposeLatency
+              - CasCommitLatency
+              - ViewLockAcquireTime
+              - ViewReadTime
+            attribute:
+              - 75thPercentile
+              - 95thPercentile
+              - OneMinuteRate
+          exclude:
+            keyspace:
+              - system
+              - system_auth
+              - system_distributed
+              - system_schema
+              - system_traces
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: Table
+            bean_regex:
+              - .*keyspace=.*
+            name:
+              - SSTablesPerReadHistogram
+              - TombstoneScannedHistogram
+              - WaitingOnFreeMemtableSpace
+            attribute:
+              - 75thPercentile
+              - 95thPercentile
+          exclude:
+            keyspace:
+              - system
+              - system_auth
+              - system_distributed
+              - system_schema
+              - system_traces
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: Table
+            bean_regex:
+              - .*keyspace=.*
+            name:
+              - ColUpdateTimeDeltaHistogram
+            attribute:
+              - Min
+              - 75thPercentile
+              - 95thPercentile
+          exclude:
+            keyspace:
+              - system
+              - system_auth
+              - system_distributed
+              - system_schema
+              - system_traces
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: Table
+            bean_regex:
+              - .*keyspace=.*
+            name:
+              - BloomFilterFalseRatio
+              - CompressionRatio
+              - KeyCacheHitRate
+              - LiveSSTableCount
+              - MaxPartitionSize
+              - MeanPartitionSize
+              - MeanRowSize
+              - MaxRowSize
+              - PendingCompactions
+              - SnapshotsSize
+            attribute:
+              - Value
+          exclude:
+            keyspace:
+              - system
+              - system_auth
+              - system_distributed
+              - system_schema
+              - system_traces
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: Table
+            bean_regex:
+              - .*keyspace=.*
+            name:
+              - CompactionBytesWritten
+              - BytesFlushed
+              - PendingFlushes
+              - LiveDiskSpaceUsed
+              - TotalDiskSpaceUsed
+              - RowCacheHitOutOfRange
+              - RowCacheHit
+              - RowCacheMiss
+            attribute:
+              - Count
+          exclude:
+            keyspace:
+              - system
+              - system_auth
+              - system_distributed
+              - system_schema
+              - system_traces
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: Cache
+            scope: KeyCache
+            name:
+              - HitRate
+            attribute:
+              - Count
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: CommitLog
+            name:
+              - PendingTasks
+              - TotalCommitLogSize
+            attribute:
+              - Value
+        - include:
+            domain: org.apache.cassandra.db
+            type: Tables
+            attribute:
+              DroppableTombstoneRatio:
+                alias: cassandra.db.droppable_tombstone_ratio
+        # Young Gen Collectors (Minor Collections)
+        - include:
+            domain: java.lang
+            type: GarbageCollector
+            name: Copy
+            attribute:
+              CollectionCount:
+                metric_type: counter
+                alias: jmx.gc.minor_collection_count
+              CollectionTime:
+                metric_type: counter
+                alias: jmx.gc.minor_collection_time
+        - include:
+            domain: java.lang
+            type: GarbageCollector
+            name: PS Scavenge
+            attribute:
+              CollectionCount:
+                metric_type: counter
+                alias: jmx.gc.minor_collection_count
+              CollectionTime:
+                metric_type: counter
+                alias: jmx.gc.minor_collection_time
+        - include:
+            domain: java.lang
+            type: GarbageCollector
+            name: ParNew
+            attribute:
+              CollectionCount:
+                metric_type: counter
+                alias: jmx.gc.minor_collection_count
+              CollectionTime:
+                metric_type: counter
+                alias: jmx.gc.minor_collection_time
+        - include:
+            domain: java.lang
+            type: GarbageCollector
+            name: G1 Young Generation
+            attribute:
+              CollectionCount:
+                metric_type: counter
+                alias: jmx.gc.minor_collection_count
+              CollectionTime:
+                metric_type: counter
+                alias: jmx.gc.minor_collection_time
+        # Old Gen Collectors (Major collections)
+        - include:
+            domain: java.lang
+            type: GarbageCollector
+            name: MarkSweepCompact
+            attribute:
+              CollectionCount:
+                metric_type: counter
+                alias: jmx.gc.major_collection_count
+              CollectionTime:
+                metric_type: counter
+                alias: jmx.gc.major_collection_time
+        - include:
+            domain: java.lang
+            type: GarbageCollector
+            name: PS MarkSweep
+            attribute:
+              CollectionCount:
+                metric_type: counter
+                alias: jmx.gc.major_collection_count
+              CollectionTime:
+                metric_type: counter
+                alias: jmx.gc.major_collection_time
+        - include:
+            domain: java.lang
+            type: GarbageCollector
+            name: ConcurrentMarkSweep
+            attribute:
+              CollectionCount:
+                metric_type: counter
+                alias: jmx.gc.major_collection_count
+              CollectionTime:
+                metric_type: counter
+                alias: jmx.gc.major_collection_time
+        - include:
+            domain: java.lang
+            type: GarbageCollector
+            name: G1 Mixed Generation
+            attribute:
+              CollectionCount:
+                metric_type: counter
+                alias: jmx.gc.major_collection_count
+              CollectionTime:
+                metric_type: counter
+                alias: jmx.gc.major_collection_time
+
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: ColumnFamily
+            bean_regex:
+              - .*keyspace=.*
+            name:
+              - ReadLatency
+              - WriteLatency
+            attribute:
+              - 75thPercentile
+              - 95thPercentile
+              - 99thPercentile
+              - OneMinuteRate
+          exclude:
+            keyspace:
+              - system
+              - system_auth
+              - system_distributed
+              - system_schema
+              - system_traces
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: ColumnFamily
+            bean_regex:
+              - .*keyspace=.*
+            name:
+              - RangeLatency
+              - CasPrepareLatency
+              - CasProposeLatency
+              - CasCommitLatency
+              - ViewLockAcquireTime
+              - ViewReadTime
+            attribute:
+              - 75thPercentile
+              - 95thPercentile
+              - OneMinuteRate
+          exclude:
+            keyspace:
+              - system
+              - system_auth
+              - system_distributed
+              - system_schema
+              - system_traces
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: ColumnFamily
+            bean_regex:
+              - .*keyspace=.*
+            name:
+              - SSTablesPerReadHistogram
+              - TombstoneScannedHistogram
+              - WaitingOnFreeMemtableSpace
+            attribute:
+              - 75thPercentile
+              - 95thPercentile
+          exclude:
+            keyspace:
+              - system
+              - system_auth
+              - system_distributed
+              - system_schema
+              - system_traces
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: ColumnFamily
+            bean_regex:
+              - .*keyspace=.*
+            name:
+              - ColUpdateTimeDeltaHistogram
+            attribute:
+              - Min
+              - 75thPercentile
+              - 95thPercentile
+          exclude:
+            keyspace:
+              - system
+              - system_auth
+              - system_distributed
+              - system_schema
+              - system_traces
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: ColumnFamily
+            bean_regex:
+              - .*keyspace=.*
+            name:
+              - BloomFilterFalseRatio
+              - CompressionRatio
+              - KeyCacheHitRate
+              - LiveSSTableCount
+              - MaxPartitionSize
+              - MeanPartitionSize
+              - MeanRowSize
+              - MaxRowSize
+              - PendingCompactions
+              - SnapshotsSize
+            attribute:
+              - Value
+          exclude:
+            keyspace:
+              - system
+              - system_auth
+              - system_distributed
+              - system_schema
+              - system_traces
+        - include:
+            domain: org.apache.cassandra.metrics
+            type: ColumnFamily
+            bean_regex:
+              - .*keyspace=.*
+            name:
+              - PendingFlushes
+              - LiveDiskSpaceUsed
+              - TotalDiskSpaceUsed
+              - RowCacheHitOutOfRange
+              - RowCacheHit
+              - RowCacheMiss
+            attribute:
+              - Count
+          exclude:
+            keyspace:
+              - system
+              - system_auth
+              - system_distributed
+              - system_schema
+              - system_traces
+        - include:
+            domain: org.apache.cassandra.db
+            type: ColumnFamilies
+            attribute:
+              DroppableTombstoneRatio:
+                alias: cassandra.db.droppable_tombstone_ratio
+    EOF
+
+    cached(:chef_run) do
+      ChefSpec::SoloRunner.new(step_into: ['datadog_monitor']) do |node|
+        node.automatic['languages'] = { python: { version: '2.7.2' } }
+
+        node.set['datadog'] = {
+          api_key: 'someapikey',
+          cassandra: {
+            version: 3,
+            instances: [
+              {
+                host: 'localhost',
+                port: 7199,
+                user: 'someuser',
+                password: 'somepass',
+                process_name_regex: '.*cassandra.*'
+              }
+            ],
+          }
+        }
+      end.converge(described_recipe)
+    end
+
+    subject { chef_run }
+
+    it_behaves_like 'datadog-agent'
+
+    it { is_expected.to include_recipe('datadog::dd-agent') }
+
+    it { is_expected.to add_datadog_monitor('cassandra') }
+
+    it 'renders expected YAML config file' do
+      expect(chef_run).to(render_file('/etc/dd-agent/conf.d/cassandra.yaml').with_content { |content|
+        expect(YAML.safe_load(content).to_json).to be_json_eql(YAML.safe_load(expected_yaml).to_json)
+      })
+    end
+  end
 end
