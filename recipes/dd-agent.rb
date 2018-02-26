e7c6591d333d25ef1706af5d7a29142c875980f1
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2018-02-26 23:09:54 +0100

    [RHEL/Amazon Linux] Add service provider hint for Agent 6 and upstart (#518)

--- recipes/dd-agent.rb
+++ recipes/dd-agent.rb
@@ -102,9 +102,18 @@ else
 end
 
 # Common configuration
+service_provider = nil
+if node['datadog']['agent6'] &&
+   (((node['platform'] == 'amazon' || node['platform_family'] == 'amazon') && node['platform_version'].to_i < 2) ||
+   (node['platform'] != 'amazon' && node['platform_family'] == 'rhel' && node['platform_version'].to_i < 7))
+  # use Upstart provider explicitly for Agent 6 on Amazon Linux < 2.0 and RHEL < 7
+  service_provider = Chef::Provider::Service::Upstart
+end
+
 service 'datadog-agent' do
   service_name node['datadog']['agent_name']
   action [agent_enable, agent_start]
+  provider service_provider unless service_provider.nil?
   if is_windows
     supports :restart => true, :start => true, :stop => true
   else
