b7dbcd2f1708dd59595445f1c519b4e4ccd47eb3
Arthur Bellal <arthur.bellal@datadoghq.com>
2018-03-21 11:05:47 -0400

    fix restart for dd-agent receipe

--- recipes/dd-agent.rb
+++ recipes/dd-agent.rb
@@ -111,6 +111,7 @@ service 'datadog-agent' do
     supports :restart => true, :status => true, :start => true, :stop => true
   end
   subscribes :restart, "template[#{agent_config_file}]", :delayed unless node['datadog']['agent_start'] == false
+  restart_command "powershell -Command \"restart-service -Force -Name datadogagent\""
   # HACK: the restart can fail when we hit systemd's restart limits (by default, 5 starts every 10 seconds)
   # To workaround this, retry once after 5 seconds, and a second time after 10 seconds
   retries 2
