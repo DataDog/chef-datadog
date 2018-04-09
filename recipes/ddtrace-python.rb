49fba5a3ed76c97a284b03a91420c831ff05ee3f
Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>
2018-04-09 21:02:08 +0200

    Added Foodcritic exclusions
    For FC059 (in .foodcritic file) and for FC079 (inline) in the recipe

--- recipes/ddtrace-python.rb
+++ recipes/ddtrace-python.rb
@@ -6,6 +6,6 @@
 # NB: This recipe is compatible with Chef <= 12 only. If you're using Chef 13 or higher
 # please refer to the README of the cookbook.
 
-easy_install_package 'ddtrace' do
+easy_install_package 'ddtrace' do # ~FC079 - This needs to be replaced before Chef 13
   version node['datadog']['ddtrace_python_version']
 end
