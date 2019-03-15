f3b9efeff6890fb54e6a5ec3bde73e99ad40b531
Remy Mathieu <remy.mathieu@datadoghq.com>
2019-03-15 10:47:48 +0100

    Keep the permissions on Administrators for Windows Agent v5.
    The ddagentuser is only for Windows Agent v6.

--- providers/monitor.rb
+++ providers/monitor.rb
@@ -10,7 +10,14 @@ end
 action :add do
   Chef::Log.debug "Adding monitoring for #{new_resource.name}"
   template ::File.join(yaml_dir, "#{new_resource.name}.yaml") do
-    unless node['platform_family'] == 'windows'
+    # On Windows Agent v5, set the permissions on files an dirs to Administrators.
+    if node['platform_family'] == 'windows'
+      unless node['datadog']['agent6']
+        owner 'Administrators'
+        rights :full_control, 'Administrators'
+        inherits false
+      end
+    else
       owner 'dd-agent'
       mode '600'
     end
