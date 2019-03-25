1a6630abaf64bc4d5f444743480a05f8aca087c3
Remy Mathieu <remy.mathieu@datadoghq.com>
2019-03-25 18:23:03 +0100

    Remove things in relation with datadog-agent-base in attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -195,19 +195,6 @@ else
   default['datadog']['agent_name'] = 'datadog-agent'
 end
 
-# DEPRECATED, will be removed after the release of datadog-agent 6.0
-# Set to true to always install datadog-agent-base (usually only installed on
-# systems with a version of Python lower than 2.6) instead of datadog-agent
-#
-# The .gsub is done because some platforms may append characters that aren't valid for a Gem::Version comparison.
-begin
-  default['datadog']['install_base'] = Gem::Version.new(node['languages']['python']['version'].gsub(/(\d\.\d\.\d).+/, '\\1')) < Gem::Version.new('2.6.0')
-rescue NoMethodError # nodes['languages']['python'] == nil
-  Chef::Log.warn 'no version of python found, please install Agent version 5.x or higher.' unless platform_family?('windows')
-rescue ArgumentError
-  Chef::Log.warn "could not parse python version string: #{node['languages']['python']['version']}"
-end
-
 # Agent Version for v5 Agents
 # To pin the version of v6 Agents, use the `agent6_version` attribute instead.
 # Default of `nil` will install latest version. On Windows, this will also upgrade to latest
