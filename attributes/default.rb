2cb2c1099115c63462592a575d6c2bf374fb56e6
Remy Mathieu <remy.mathieu@datadoghq.com>
2019-05-21 16:59:23 +0200

    Better comment for windows_ddagentuser_name/password attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -199,9 +199,8 @@ end
 # Since 6.11.0, the Datadog Agent creates/uses a custom user to run on Windows.
 # Set `windows_ddagentuser_name` using the format `<domain>\<user>` to provide a
 # specific username and `windows_ddagentuser_password` to provide a specific password.
-# You can set those values here, in role/environment/node or set in your
-# node `run_state` under the keys `['datadog']['windows_ddagentuser_name']` and
-# `['datadog']['windows_ddagentuser_password']`.
+# You can set these 2 values as node attributes or on `node.run_state` under
+# the keys `['datadog']['windows_ddagentuser_name']` and `['datadog']['windows_ddagentuser_password']`
 default['datadog']['windows_ddagentuser_name'] = nil
 default['datadog']['windows_ddagentuser_password'] = nil
 
