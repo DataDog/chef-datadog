3bcd9b6ff2a7fb1356bfe6b35175a65db75e678f
Remy Mathieu <remy.mathieu@datadoghq.com>
2019-05-29 11:13:34 +0200

    Add test cookbooks for the datadog_integration resource.

---
+++ test/cookbooks/test/metadata.rb
@@ -0,0 +1,7 @@
+name              'test'
+maintainer        'Datadog'
+maintainer_email  'package@datadoghq.com'
+license           'Apache-2.0'
+version           '0.0.1'
+description       'Tests cookbooks'
+depends           'datadog'
