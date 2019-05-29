aa9ff58dbbb427a8763c0ca16eb62722ab136f99
Remy Mathieu <remy.mathieu@datadoghq.com>
2019-05-29 10:19:18 +0200

    Integration resource converges only when something happened.

--- Berksfile
+++ Berksfile
@@ -4,4 +4,5 @@ metadata
 
 group :integration do
   cookbook 'sudo'
+  cookbook 'test', path: './test/cookbooks/test' # Integration test
 end
--- resources/integration.rb
+++ resources/integration.rb
@@ -4,10 +4,11 @@
 # The datadog_resource must be used on a system where the datadog-agent has
 # already been setup.
 
+require 'chef/mixin/shell_out'
+
 default_action :install
 
-property :name, String, name_attribute: true
-
+property :property_name, String, name_property: true
 property :version, String, required: true
 
 action :install do
@@ -16,11 +17,24 @@ action :install do
     return
   end
 
-  Chef::Log.debug("Getting integration #{new_resource.name}")
+  Chef::Log.debug("Getting integration #{new_resource.property_name}")
 
   execute 'integration install' do
-    command   "\"#{agent_exe_filepath}\" integration install #{new_resource.name}==#{new_resource.version}"
+    command   "\"#{agent_exe_filepath}\" integration install #{new_resource.property_name}==#{new_resource.version}"
     user      'dd-agent' unless node['platform_family'] == 'windows'
+
+    not_if {
+      output = shell_out("#{agent_exe_filepath} integration show #{new_resource.property_name}").stdout
+      if output.match(/Installed version/) then
+        property_name = output.match(/Package (.+):$/)[1].strip
+        version       = output.match(/Installed version: (.+)$/)[1].strip
+        # Same version already installed? Do not reinstall.
+        property_name == new_resource.property_name and version == new_resource.version
+      else
+        # Nothing installed yet, we want to install/
+        false
+      end
+    }
   end
 end
 
@@ -30,11 +44,16 @@ action :remove do
     return
   end
 
-  Chef::Log.debug("Removing integration #{new_resource.name}")
+  Chef::Log.debug("Removing integration #{new_resource.property_name}")
 
   execute 'integration remove' do
-    command   "\"#{agent_exe_filepath}\" integration remove #{new_resource.name}"
+    command   "\"#{agent_exe_filepath}\" integration remove #{new_resource.property_name}"
     user      'dd-agent' unless node['platform_family'] == 'windows'
+
+    only_if {
+      output = shell_out("#{agent_exe_filepath} integration show #{new_resource.property_name}").stdout
+      output.match(/Installed version/)
+    }
   end
 end
 
