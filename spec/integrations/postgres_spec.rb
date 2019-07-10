104b52c1de1de738968e111fb8c0b72c03819da3
Rémy Mathieu <remy.mathieu@datadoghq.com>
2019-07-10 14:44:30 +0200

    Add CHANGELOG entry for 3.1.0 (#633)
    * Add CHANGELOG entry for 3.1.0
    * Typo.
    * Fix spec tests.

--- spec/integrations/postgres_spec.rb
+++ spec/integrations/postgres_spec.rb
@@ -165,7 +165,7 @@ describe 'datadog::postgres' do
       ChefSpec::SoloRunner.new(step_into: ['datadog_monitor']) do |node|
         node.automatic['languages'] = { python: { version: '2.7.2' } }
 
-        node.set['datadog'] = {
+        node.normal['datadog'] = {
           api_key: 'someapikey',
           postgres: {
             instances: [
@@ -188,7 +188,7 @@ describe 'datadog::postgres' do
     it { is_expected.to add_datadog_monitor('postgres') }
 
     it 'renders expected YAML config file' do
-      expect(chef_run).to(render_file('/etc/dd-agent/conf.d/postgres.yaml').with_content { |content|
+      expect(chef_run).to(render_file('/etc/datadog-agent/conf.d/postgres.yaml').with_content { |content|
         expect(YAML.safe_load(content).to_json).to be_json_eql(YAML.safe_load(expected_yaml).to_json)
       })
     end
