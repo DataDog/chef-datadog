6978474e71a86155229a7e81bc223b68e441a03c
Dan May <daniel.may@7digital.com>
2019-11-06 18:13:15 +0000

    Fix attribute quotes

--- attributes/network.rb
+++ attributes/network.rb
@@ -2,7 +2,7 @@ default['datadog']['network']['instances'] = [
   {
     'collect_connection_state' => 'false',
     'collect_count_metrics' => 'false',
-    'combine_connection_states' => "true",
+    'combine_connection_states' => 'true',
     'excluded_interfaces' => ['lo', 'lo0']
   }
 ]
