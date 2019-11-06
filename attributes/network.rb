a2a1dee9d117bd3721cb7c84c410e3c06a4b5df5
Dan May <daniel.may@7digital.com>
2019-11-06 17:59:26 +0000

    Allow configuration of combine_connection_states and collect_count_metrics in network.yaml

--- attributes/network.rb
+++ attributes/network.rb
@@ -1,6 +1,8 @@
 default['datadog']['network']['instances'] = [
   {
     'collect_connection_state' => 'false',
+    'collect_count_metrics' => 'false',
+    'combine_connection_states' => "true",
     'excluded_interfaces' => ['lo', 'lo0']
   }
 ]
--- recipes/network.rb
+++ recipes/network.rb
@@ -5,6 +5,8 @@ include_recipe 'datadog::dd-agent'
 # node.override['datadog']['network']['instances'] = [
 #   {
 #     :collect_connection_state => "false",
+#     :collect_count_metrics => "false",
+#     :combine_connection_states => "true",
 #     :excluded_interfaces => ["lo","lo0"]
 #   },
 # ]
