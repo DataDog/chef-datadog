1829e1bb7fa1057c45a22a1d88827f5d6edd3ae2
Albert Vaca <albert.vaca@datadoghq.com>
2019-11-07 13:23:00 +0100

    Windows hash check style fixes so it passes linter

--- recipes/_install-windows.rb
+++ recipes/_install-windows.rb
@@ -72,18 +72,18 @@ remote_file temp_file do
 end
 
 unsafe_hashsums = [
-    '928b00d2f952219732cda9ae0515351b15f9b9c1ea1d546738f9dc0fda70c336',
-    '78b2bb2b231bcc185eb73dd367bfb6cb8a5d45ba93a46a7890fd607dc9188194'
+  '928b00d2f952219732cda9ae0515351b15f9b9c1ea1d546738f9dc0fda70c336',
+  '78b2bb2b231bcc185eb73dd367bfb6cb8a5d45ba93a46a7890fd607dc9188194'
 ]
 
-ruby_block "validate_safe_msi" do
+ruby_block 'validate_safe_msi' do
   block do
     require 'digest'
 
     sha256 = Digest::SHA256.file(temp_file)
 
-    raise "The file downloaded matches a known unsafe MSI - Agent versions 6.14.0/1 have been blacklisted. please use a different release. "\
-        "See http://dtdg.co/win-614-fix" if unsafe_hashsums.include? sha256.hexdigest
+    raise 'The file downloaded matches a known unsafe MSI - Agent versions 6.14.0/1 have been blacklisted. please use a different release. '\
+        'See http://dtdg.co/win-614-fix' if unsafe_hashsums.include? sha256.hexdigest
   end
 
   action :nothing
