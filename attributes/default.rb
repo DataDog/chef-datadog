6fb15125c5997839ad16e74e94702664220db833
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2019-11-14 18:58:01 -0500

    [attributes] bump default to 900s to give more time for uninstalls on tiny instances

--- attributes/default.rb
+++ attributes/default.rb
@@ -193,7 +193,7 @@ default['datadog']['windows_blacklist_silent_fail'] = false
 
 # Attribute to specify timeout in seconds on MSI operations (install/uninstall)
 # Default should suffice, but provides a knob in case instances with limited resources timeout.
-default['datadog']['windows_msi_timeout'] = 600
+default['datadog']['windows_msi_timeout'] = 900
 
 # Agent installer checksum
 # Expected checksum to validate correct agent installer is downloaded (Windows only)
