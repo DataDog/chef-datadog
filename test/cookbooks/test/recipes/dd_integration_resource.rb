8f2e6e8f22c5932c1ebd84800e52e9c65e9f6e1a
Julien Lebot <julien.lebot@datadoghq.com>
2020-08-12 18:36:32 +0200

    Add integration test

--- test/cookbooks/test/recipes/dd_integration_resource.rb
+++ test/cookbooks/test/recipes/dd_integration_resource.rb
@@ -15,3 +15,21 @@ datadog_integration 'datadog-aerospike' do
   action :install
   version '1.2.0'
 end
+
+datadog_integration 'datadog-elastic' do
+  action :remove
+  notifies :restart, 'service[datadog-agent]'
+end
+# This time Chef should mention "up to date"
+datadog_integration 'datadog-elastic' do
+  action :remove
+end
+datadog_integration 'datadog-elastic' do
+  action :install
+  version '1.11.0'
+end
+# This time Chef should mention "up to date"
+datadog_integration 'datadog-elastic' do
+  action :install
+  version '1.11.0'
+end
