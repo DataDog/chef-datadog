7f09dbb4c1a3e1ea796711d925cc8e069f4db6ee
nils kuehme <nils.kuehme@awin.com>
2020-10-21 12:45:47 +0200

    Implement Mongo Authentication
    The server attribute is deprecated according to Agent 7.22.1 documentation. Authentication with username and password to Mongo is possible now.

--- spec/integrations/mongo_spec.rb
+++ spec/integrations/mongo_spec.rb
@@ -4,7 +4,8 @@ describe 'datadog::mongo' do
     init_config:
 
     instances:
-      - hosts: localhost:27017
+      - hosts:
+          - localhost:27017
         username: someuser
         password: somepassword
         timeout: 60
