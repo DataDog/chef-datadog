4db2028f29553445bac3eb7c0a4a69ee178b687b
Albert Vaca Cintora <albert.vaca@datadoghq.com>
2021-02-11 12:15:40 +0100

    Kafka recipe: allow setting any conf

--- spec/integrations/kafka_spec.rb
+++ spec/integrations/kafka_spec.rb
@@ -622,4 +622,84 @@ describe 'datadog::kafka' do
       })
     end
   end
+
+  context 'custom conf' do
+    expected_yaml = <<-EOF
+    logs: ~
+    instances:
+            - host: localhost
+              port: 7199
+              name: fasdfas
+              user: someuser
+              password: somepass
+              tags:
+                key: value
+
+    init_config:
+      is_jmx: true
+
+      conf:
+        - include:
+            domain: 'kafka.producer'
+            bean_regex: 'kafka\\.producer:type=ProducerRequestMetrics,name=ProducerRequestRateAndTimeMs,clientId=.*'
+            attribute:
+              Count:
+                metric_type: rate
+                alias: kafka.producer.request_rate
+    EOF
+
+    cached(:chef_run) do
+      ChefSpec::SoloRunner.new(
+        platform: 'ubuntu',
+        version: '16.04',
+        step_into: ['datadog_monitor']
+      ) do |node|
+        node.automatic['languages'] = { python: { version: '2.7.2' } }
+
+        node.normal['datadog'] = {
+          api_key: 'someapikey',
+          kafka: {
+            conf: [
+              {
+                include: {
+                  domain: 'kafka.producer',
+                  bean_regex: 'kafka\\.producer:type=ProducerRequestMetrics,name=ProducerRequestRateAndTimeMs,clientId=.*',
+                  attribute: {
+                    Count: {
+                      metric_type: 'rate',
+                      alias: 'kafka.producer.request_rate'
+                    }
+                  }
+                }
+              }
+            ],
+            instances: [
+              {
+                host: 'localhost',
+                port: 7199,
+                name: 'fasdfas',
+                user: 'someuser',
+                password: 'somepass',
+                tags: { key: 'value' }
+              }
+            ]
+          }
+        }
+      end.converge(described_recipe)
+    end
+
+    subject { chef_run }
+
+    it_behaves_like 'datadog-agent'
+
+    it { is_expected.to include_recipe('datadog::dd-agent') }
+
+    it { is_expected.to add_datadog_monitor('kafka') }
+
+    it 'renders expected YAML config file' do
+      expect(chef_run).to(render_file('/etc/datadog-agent/conf.d/kafka.d/conf.yaml').with_content { |content|
+        expect(YAML.safe_load(content).to_json).to be_json_eql(YAML.safe_load(expected_yaml).to_json)
+      })
+    end
+  end
 end
