56c0f612888303c49741c7ff9f5cdbebc83073ea
Nils Kühme <nils.kuehme@awin.com>
2022-03-07 20:53:16 +0100

    Kafka Consumer Additions (#840)
    Two options have been added to kafka_consumer.rb:
    - monitor_unlisted_consumer_groups
    - kafka_consumer_offsets

--- recipes/kafka_consumer.rb
+++ recipes/kafka_consumer.rb
@@ -6,8 +6,18 @@ include_recipe '::dd-agent'
 # node.datadog.kafka_consumer.instances = [
 #   {
 #     :kafka_connect_str => "localhost:19092",
+#     :consumer_groups => {
+#     :my_consumer => {
+#       :my_topic => [<partition>, <partition>, <partition>, <partition>]
+#     } ,
+#     # optional -if consumer_groups is set do false no matter of it's set to true or not.
+#     # false is the default configuration, but since there is a dependency on consumer_groups
+#     # it will be added to conf.yaml as false.
+#     :monitor_unlisted_consumer_groups >> true,
 #     :zk_connect_str => "localhost:2181",
-#     :zk_prefix => "/0.8"
+#     :zk_prefix => "/0.8",
+#     # optional -if it's set to false it will no added to conf.yaml because false is default.
+#     :kafka_consumer_offsets => true
 #   }
 # ]
 
