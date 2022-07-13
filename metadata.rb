1ae7c1b058d399b4e6ecf7387e5a80ac925bb4bd
Axel Amigo Arnold <axl89@users.noreply.github.com>
2022-07-13 18:57:39 +0200

    Removed chef_handler cookbook as its EOL
    See https://github.com/chef-boneyard/chef_handler#deprecation:
    
    The chef_handler resource from this cookbook is now shipping as part of Chef 14. With the inclusion of this resource into Chef itself we are now deprecating this cookbook. It will continue to function for Chef 13 users, but will not be updated.

--- metadata.rb
+++ metadata.rb
@@ -22,6 +22,5 @@ issues_url       'https://github.com/DataDog/chef-datadog/issues'
   supports os
 end
 
-depends    'chef_handler', '>= 1.2'
 depends    'apt' # Use '< 6.0.0' with Chef < 12.9
 depends    'yum', '>= 3.0' # Use '< 5.0' with Chef < 12.14
