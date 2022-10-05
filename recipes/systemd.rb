fabe8efd380c1c556dc05c3c6b11ea4292da7c84
nilskuehme <nils.kuehme@awin.com>
2022-10-05 10:35:58 +0200

    Adjusted systemd.rb to use plural version

--- recipes/systemd.rb
+++ recipes/systemd.rb
@@ -1,48 +1,48 @@
 include_recipe '::dd-agent'
 
 # Monitor Systemd
-# It works for all unit types like target, service, socket, device, mount, automount, swap, path, timer, snapshot, slice, scope, busname
-# node.default['datadog']['systemd']['instances'] = [
-#   {
-#     'unit_names' => [
-#       'myservice1.service',
-#       'myservice2.service',
-#       'mysocket.socket',
-#       'mytimer.timer'
-#     ],
-#     'substate_status_mapping' => [
-#       'service' => [
-#         'myservice1' => {
-#           'running' => 'ok',
-#           'exited' => 'critical'
-#         },
-#         'myservice2' => {
-#           'plugged' => 'ok',
-#           'mounted' => 'ok',
-#           'running' => 'ok',
-#           'exited' => 'critical',
-#           'stopped' => 'critical'
-#         }
-#       ],
-#       'socket' => [
-#         'mysocket' => {
-#           'running' => 'ok',
-#           'exited' => 'critical'
-#         }
-#       ],
-#       'timer' => [
-#         'mytimer' => {
-#          'running' => 'ok',
-#          'exited' => 'critical'
-#         }
-#       ]
-#     ],
-#     'tags' => [
-#       'mykey1:myvalue1',
-#       'mykey2:myvalue2'
-#     ]
-#   }
-# ]
+# It works for all unit types like targets, services, sockets, devices, mounts, automounts, swaps, paths, timers, snapshots, slices, scopes, busnames
+node.default['datadog']['systemd']['instances'] = [
+  {
+    'unit_names' => [
+      'myservice1.service',
+      'myservice2.service',
+      'mysocket.socket',
+      'mytimer.timer'
+    ],
+    'substate_status_mapping' => [
+      'services' => [
+        'myservice1' => {
+          'running' => 'ok',
+          'exited' => 'critical'
+        },
+        'myservice2' => {
+          'plugged' => 'ok',
+          'mounted' => 'ok',
+          'running' => 'ok',
+          'exited' => 'critical',
+          'stopped' => 'critical'
+        }
+      ],
+      'sockets' => [
+        'mysocket' => {
+          'running' => 'ok',
+          'exited' => 'critical'
+        }
+      ],
+      'timers' => [
+        'mytimer' => {
+         'running' => 'ok',
+         'exited' => 'critical'
+        }
+      ]
+    ],
+    'tags' => [
+      'mykey1:myvalue1',
+      'mykey2:myvalue2'
+    ]
+  }
+]
 
 datadog_monitor 'systemd' do
   instances node['datadog']['systemd']['instances']
