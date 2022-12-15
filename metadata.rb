613a2cf878022b2f75655a5a57e3cbcbd01dba32
Alexandre Menasria <alexandre.menasria@datadoghq.com>
2022-12-15 16:03:31 +0100

    Don't use chef_handler cookbook for Chef >= 14

--- metadata.rb
+++ metadata.rb
@@ -22,6 +22,10 @@ issues_url       'https://github.com/DataDog/chef-datadog/issues'
   supports os
 end
 
-depends    'chef_handler', '>= 1.2'
+if Chef::VERSION < Gem::Version.new(14)
+  # The chef_handler cookbook is shipped as part of Chef >= 14,
+  # so from Chef >= 14 chef_handler cookbook is deprecated.
+  depends    'chef_handler', '>= 1.2'
+end
 depends    'apt' # Use '< 6.0.0' with Chef < 12.9
 depends    'yum', '>= 3.0' # Use '< 5.0' with Chef < 12.14
