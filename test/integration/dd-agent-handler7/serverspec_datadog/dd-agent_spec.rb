7c9ca18ccc7dc7ff752d8c373f88566cfe77bedf
Pythyu <antoine.zellmeyer@datadoghq.com>
2024-04-23 14:56:20 +0200

    feat(lint): fix linting issue

--- test/integration/dd-agent-handler7/serverspec_datadog/dd-agent_spec.rb
+++ test/integration/dd-agent-handler7/serverspec_datadog/dd-agent_spec.rb
@@ -49,12 +49,12 @@ describe command('rpm -q gpg-pubkey-fd4bf915'), :if => os[:family] == 'redhat' d
   its(:stdout) { should contain 'gpg-pubkey-fd4bf915' }
 end
 
-describe command('rpm -q gpg-pubkey-e09422b3'), :if => os[:family] == 'redhat' && agent_minor_version && agent_minor_version <= 35) do
+describe command('rpm -q gpg-pubkey-e09422b3'), :if => os[:family] == 'redhat' && agent_minor_version && agent_minor_version <= 35 do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should contain 'gpg-pubkey-e09422b3' }
 end
 
-describe command('rpm -q gpg-pubkey-e09422b3'), :if => os[:family] == 'redhat' && agent_minor_version && agent_minor_version > 35) do
+describe command('rpm -q gpg-pubkey-e09422b3'), :if => os[:family] == 'redhat' && agent_minor_version && agent_minor_version > 35 do
   its(:exit_status) { should eq 1 }
   its(:stdout) { should_not contain 'gpg-pubkey-e09422b3' }
 end
