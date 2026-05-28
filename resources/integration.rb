d159757c17964d9be2e36f91479f7692e12a647b
jeremypope <jeremy.th.pope@gmail.com>
2026-05-28 11:34:48 -0400

    Remove empty line

--- resources/integration.rb
+++ resources/integration.rb
@@ -39,7 +39,6 @@ action :install do
 
   Chef::Log.debug("Getting integration #{new_resource.property_name}")
 
-
   install_params = if new_resource.local_wheel
                      # The Agent cannot perform any verification on local wheels.
                      "--local-wheel #{new_resource.local_wheel}"
