b21a97865d715097f1cf828a3d8be8bef53e16f9
jeremypope <jeremy.th.pope@gmail.com>
2026-05-28 17:39:34 -0400

    Style fix for spec test again

--- spec/integrations/integration_spec.rb
+++ spec/integrations/integration_spec.rb
@@ -76,7 +76,7 @@ describe 'datadog_integration' do
   context 'with local wheel file integration that is not installed' do
     before do
       allow(File).to receive(:exist?).and_call_original
-      allow(File).to receive(:exist?).with("/path/to/foo-bar.whl").and_return(true)
+      allow(File).to receive(:exist?).with('/path/to/foo-bar.whl').and_return(true)
     end
     stubs_for_resource('execute[integration install]') do |resource|
       allow(resource).to receive_shell_out('/opt/datadog-agent/bin/agent/agent integration show -q foo-bar')
@@ -93,7 +93,7 @@ describe 'datadog_integration' do
   context 'with local wheel file integration that is already installed' do
     before do
       allow(File).to receive(:exist?).and_call_original
-      allow(File).to receive(:exist?).with("/path/to/foo-bar.whl").and_return(true)
+      allow(File).to receive(:exist?).with('/path/to/foo-bar.whl').and_return(true)
     end
     stubs_for_resource('execute[integration install]') do |resource|
       allow(resource).to receive_shell_out('/opt/datadog-agent/bin/agent/agent integration show -q foo-bar')
@@ -111,7 +111,7 @@ describe 'datadog_integration' do
   context 'with local wheel file integration that is installed with an older version' do
     before do
       allow(File).to receive(:exist?).and_call_original
-      allow(File).to receive(:exist?).with("/path/to/foo-bar.whl").and_return(true)
+      allow(File).to receive(:exist?).with('/path/to/foo-bar.whl').and_return(true)
     end
     stubs_for_resource('execute[integration install]') do |resource|
       allow(resource).to receive_shell_out('/opt/datadog-agent/bin/agent/agent integration show -q foo-bar')
